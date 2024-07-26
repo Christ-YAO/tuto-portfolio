8333e583c3734497bf575bebe5617569806c5d80
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-26 09:18:39 +0000

    instal geist font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Anek_Telugu } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const AnekTelugu = Anek_Telugu({
+  subsets: ["latin"],
+  variable: "--font-caption",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +22,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          GeistSans.variable,
+          GeistMono.variable,
+          AnekTelugu.variable,
+          "h-full"
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
