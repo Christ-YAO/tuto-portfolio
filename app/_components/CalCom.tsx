e5a4de4cd8de6f350b87e53997fa1e36a5e2bed7
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-29 10:24:31 +0000

    Calcom

--- app/_components/CalCom.tsx
+++ app/_components/CalCom.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import Section from "./Section";
+import Link from "next/link";
 // import { Booker } from "@calcom/atoms";
 
 export default function CalCom() {
@@ -13,6 +15,16 @@ export default function CalCom() {
         10 minutes to talk about your fututre project.
       </p>
       {/* <Booker username={"melvynx"} eventSlug="call-with-melvynx" /> */}
+      <Image
+        src="/Calcom.png"
+        alt="showcase"
+        width={400}
+        height={200}
+        className="w-full h-auto mt-6 lg:mt-8"
+      />
+      <h3 className="text-[14px] font-light text-center my-6 text-neutral-600">
+        Call with yao | <Link href="https://cal.com/melvynx" className="hover:underline hover:text-white transition-all underline-offset-2 font-semibold">Cal.com</Link>
+      </h3>
     </Section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import CalCom from "./_components/CalCom";
 import Footer from "./_components/Footer";
 import Hero from "./_components/Hero";
+import { Nav } from "./_components/Nav";
 import Showcase from "./_components/Showcase";
 import Work from "./_components/Work";
 
 export default function Home() {
   return (
     <main className="h-full text-base">
+      <Nav />
       <Hero />
       <Work />
       <Showcase />
-      {/* <CalCom /> */}
+      <CalCom />
       <Footer />
     </main>
   );
