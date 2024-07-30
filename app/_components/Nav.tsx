d3830f081f9c5eb5e1b7aa615442201209cc00ab
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-30 09:42:57 +0000

    change some thing to the nav bar

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -12,27 +12,26 @@ export const Nav = () => {
 
   return (
     // <Headroom>
-      <header className="top-0 py-4 backdrop-blur-md z-50 fixed w-full">
+      <header className="top-0 py-6 backdrop-blur-md z-50 fixed w-full">
         <div className="flex justify-between items-baseline max-w-2xl m-auto">
-          <Link
-            href={"/"}
-            className="text-[16px] font-black px-1 sm:px-3 relative rounded text-accent-foreground hover:bg-accent transition-all italic"
+          <h5
+            className="text-[16px] font-black px-1 sm:px-3 relative rounded-sm dark:text-neutral-300 bg-accent/50 italic"
           >
-            Christ yao
-          </Link>
-          <div className="flex items-center gap-2 text-primary font-semibold text-[13px]">
+            christ yao
+          </h5>
+          <div className="flex items-center gap-2 text-primary dark:dark:text-neutral-300 text-[13px]">
             <Link
               href={"/#skills"}
-              className={clsx("px-2 py-1 transition font-extralight", {
-                "font-medium": pathname === "/",
+              className={clsx("px-2 transition dark:font-extralight hover:bg-accent rounded", {
+                "font-bold dark:font-semibold bg-accent/50": pathname === "/",
               })}
             >
               <span className="font-mono">0.</span> Skills
             </Link>
             <Link
               href={"/tools"}
-              className={clsx("px-2 py-1 transition font-extralight", {
-                "font-medium": pathname === "/tools",
+              className={clsx("px-2 transition dark:font-extralight hover:bg-accent rounded", {
+                "font-bold dark:font-semibold bg-accent/50": pathname === "/tools",
               })}
             >
               <span className="font-mono">1.</span> tools
