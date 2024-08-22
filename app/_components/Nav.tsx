9d287acbb9b7f540787040cb40238aac0af02f3c
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-08-22 10:54:39 +0000

    Add scroll effet when scrolling up and down

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -6,19 +6,23 @@ import clsx from "clsx";
 // import Headroom from "react-headroom";
 import Section from "./Section";
 import { ThemeToggle } from "@/components/ui/theme/ThemeToggle";
+import useDetectScroll, { Direction } from "@smakss/react-scroll-direction";
+import { cn } from "@/lib/utils";
 
 export const Nav = () => {
   const pathname = usePathname();
+  const { scrollDir } = useDetectScroll();
 
   return (
     // <Headroom>
-      <header className="top-0 py-6 backdrop-blur-md z-50 fixed w-full">
-        <div className="flex justify-between items-baseline max-w-2xl m-auto">
-          <h5
-            className="text-[16px] font-black px-1 sm:px-3 relative rounded-sm dark:text-neutral-300 bg-accent/50 italic"
-          >
-            christ yao
-          </h5>
+    <header className={cn("top-0 py-6 backdrop-blur-lg z-50 fixed w-full overflow-hidden transition-all", scrollDir === Direction.Down && "py-4")}>
+      <div className="flex justify-between items-baseline max-w-2xl m-auto relative">
+        <h5
+          className="text-[16px] font-black px-1 sm:px-3 relative rounded-sm dark:text-neutral-300 bg-accent/50 italic"
+        >
+          christ yao
+        </h5>
+        <div className={cn("absolute right-0 top-1/2 -translate-y-1/2 transition-all duration-300", scrollDir === Direction.Down && "absolute right-0 -top-10 opacity-0")}>
           <div className="flex items-center gap-2 text-primary dark:dark:text-neutral-300 text-[13px]">
             <Link
               href={"/#skills"}
@@ -39,7 +43,8 @@ export const Nav = () => {
             <ThemeToggle />
           </div>
         </div>
-      </header>
+      </div>
+    </header>
     // </Headroom>
   );
 };
