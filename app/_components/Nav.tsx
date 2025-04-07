2e9b089d6d68e61c0c996ff7082c560cd106788b
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-04-07 20:23:16 +0000

    Nav bar updated

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -15,32 +15,95 @@ export const Nav = () => {
 
   return (
     // <Headroom>
-    <header className={cn("top-0 py-6 backdrop-blur-lg z-50 fixed w-full overflow-hidden transition-all", scrollDir === Direction.Down && "py-4")}>
-      <div className="flex justify-between items-baseline max-w-2xl m-auto relative">
-        <h5
-          className="text-[16px] font-black px-1 sm:px-3 relative rounded-sm dark:text-neutral-300 bg-accent/50 italic"
+    <header
+      className={cn(
+        "top-0 py-6 backdrop-blur-3xl z-50 fixed w-full overflow-hidden transition-all px-4 xl:px-0"
+      )}
+    >
+      <div className="flex justify-between items-baseline max-w-6xl m-auto relative">
+        <div
+          className={cn(
+            "relative transition-all duration-500",
+            scrollDir === Direction.Down && "ml-8 font-bold"
+          )}
         >
-          christ yao
-        </h5>
-        <div className={cn("absolute right-0 top-1/2 -translate-y-1/2 transition-all duration-300", scrollDir === Direction.Down && "absolute right-0 -top-10 opacity-0")}>
-          <div className="flex items-center gap-2 text-primary dark:dark:text-neutral-300 text-[13px]">
-            <Link
-              href={"/#skills"}
-              className={clsx("px-2 transition dark:font-extralight hover:bg-accent rounded", {
-                "font-bold dark:font-semibold bg-accent/50": pathname === "/",
-              })}
+          <Link
+            href={"/"}
+            className={cn(
+              "text-xl group dark:font-medium relative transition-all duration-500"
+            )}
+          >
+            T
+            <span
+              className={cn(
+                "absolute opacity-0 group-hover:relative group-hover:opacity-100 transition-all duration-300 ease-in-out",
+                scrollDir === Direction.Down && "relative opacity-100"
+              )}
             >
-              <span className="font-mono">0.</span> Skills
-            </Link>
-            <Link
-              href={"/tools"}
-              className={clsx("px-2 transition dark:font-extralight hover:bg-accent rounded", {
-                "font-bold dark:font-semibold bg-accent/50": pathname === "/tools",
-              })}
+              héo
+            </span>{" "}
+            <span
+              className={cn(
+                "absolute left-3 group-hover:left-[56px] transition-all duration-300 ease-in-out",
+                scrollDir === Direction.Down && "left-[56px]"
+              )}
             >
-              <span className="font-mono">1.</span> tools
-            </Link>
-            <ThemeToggle />
+              D
+            </span>
+            <span
+              className={cn(
+                "absolute opacity-0 group-hover:relative group-hover:opacity-100 transition-all duration-300 ease-in-out ml-5",
+                scrollDir === Direction.Down && "relative opacity-100 ml-[20px]"
+              )}
+            >
+              iame
+            </span>
+            <span
+              className={cn(
+                "absolute left-7 group-hover:left-[120px] transition-all dura delay-150 ease-in-out",
+                scrollDir === Direction.Down && "left-[120px]"
+              )}
+            >
+              .
+            </span>
+          </Link>
+        </div>
+        <div
+          className={cn(
+            "absolute right-0 top-1/2 -translate-y-1/2 transition-all duration-300",
+            scrollDir === Direction.Down && "absolute right-0 -top-10 opacity-0"
+          )}
+        >
+          <div className="flex items-center gap-2 text-primary dark:dark:text-neutral-300 text-[14px]">
+            <a
+              href={"/#services"}
+              className={clsx(
+                "py-2 px-4 transition font-medium hover:bg-accent rounded",
+                {
+                  "font-extrabold bg-accent/50": pathname === "/#services",
+                }
+              )}
+            >
+              <span className="font-mono">0.</span> Services
+            </a>
+            <a
+              href={"/#about"}
+              className={clsx(
+                "py-2 px-4 transition font-medium hover:bg-accent rounded mr-10",
+                {
+                  "font-extrabold bg-accent/50": pathname === "/#about",
+                }
+              )}
+            >
+              <span className="font-mono">1.</span> A Propos
+            </a>
+            <a
+              href={"/#contact"}
+              className="cursor-pointer font-medium rounded-4xl bg-[#2c99b7] text-white hover:brightness-90 hover:bg-[#2c99b7] transition-all px-4 py-2"
+            >
+              Prendre contact
+            </a>
+            {/* <ThemeToggle /> */}
           </div>
         </div>
       </div>
