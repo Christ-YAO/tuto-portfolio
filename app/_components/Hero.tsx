06e92791c906b3ed85839379a3e995cc09057d60
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-04-28 23:04:22 +0000

    Hero text

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -21,16 +21,18 @@ export default function Hero() {
       </div>
       <h1 className="text-4xl lg:text-5xl font-bold">Hey, I am Yao 🫡</h1>
       <p className="text-[15px] leading-7">
-        I help{" "}
+        I{" "}
         <Code>
-          <CustomIcon className="inline" name="youtube" size={16} /> Youtubers
+          <CustomIcon className="inline" name="code" size={16} /> build
         </Code>{" "}
-        grow on Youtube with captivating{" "}
+        modern web applications with{" "}
         <Code>
-          <CustomIcon className="inline" name="videoEditing" size={16} /> video
-          editing
+          <CustomIcon className="inline" name="reactjs" size={16} /> React.js
+        </Code>{" "}
+        &{" "}
+        <Code>
+          <CustomIcon className="inline" name="nextjs" size={14.5} /> Next.js
         </Code>
-        .
       </p>
       <div className="mt-4 lg:mt-6 flex items-center flex-wrap gap-4">
         <Button size={"lg"} className="rounded w-fit">
@@ -70,7 +72,10 @@ export default function Hero() {
       >
         <div
           className="relative left-[calc(10%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#22c55e] to-[#059669] opacity-30 sm:left-[calc(10%+36rem)] sm:w-[72.1875rem]"
-          style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
         ></div>
       </div>
       <div
@@ -79,7 +84,10 @@ export default function Hero() {
       >
         <div
           className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#de1414] to-[#2f0df2] t opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
         ></div>
       </div>
     </Section>
